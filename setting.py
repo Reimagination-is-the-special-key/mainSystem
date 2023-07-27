36bea6e1bc1ae856dfa19724e672b877035921e9
herb <69900670+WhiteHerb@users.noreply.github.com>
2023-07-27 09:59:00 +0900

    grid 도입

--- setting.py
+++ setting.py
@@ -1,17 +1,68 @@
+from typing import Any, List
+
 import pygame as pg
 
-from system import Chapter
+
+# from system import Chapter
+
+
+class Btn(pg.sprite.Sprite):
+    def __init__(self, type_: int, group, call):
+        super(Btn, self).__init__()
+        pg.sprite.Sprite.__init__(self, group)
+        self.call = call
+        # self.image = pg.image.load(f'./data/imgs/btn_{type_}.png')
+        self.image = pg.Surface((10, 10))
+        self.image.set_colorkey(pg.Color(255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.x = self.rect.x
+        self.y = self.rect.y
+
+    def update(self, *args: Any, **kwargs: Any) -> None:
+        self.x = self.rect.x
+        self.y = self.rect.y
 
 
 class Settings(pg.sprite.Sprite):
 
-    def __init__(self, chapter: Chapter):
-        pg.sprite.Sprite.__init__(self)
+    def __init__(self, chapter):
+        super(Settings, self).__init__()
         self.chapter = chapter
+        # self.image = pg.image.load('./data/imgs/setting.png')
+        self.image = pg.Surface((30, 30))
+        self.rect = self.image.get_rect()
+        self.x = self.rect.x
+        self.y = self.rect.y
+        self.btnGroup = pg.sprite.Group()
+        self.btnList: List[Btn] = []
+        self.initbtn()
+
+    def initbtn(self):
+        btn1 = Btn(1, self.btnGroup, self.save)
+        btn2 = Btn(2, self.btnGroup, self.quit)
+        btn3 = Btn(3, self.btnGroup, None)
+        self.btnList.append(btn1)
+        self.btnList.append(btn2)
+        self.btnList.append(btn3)
 
     def save(self):
         self.chapter.save()
 
+    def open_dev(self):
+        self.image = pg.image.load('./data/imgs/dev.png')
+
+    def update(self):
+        self.x = self.rect.x
+        self.y = self.rect.y
+        mouse = pg.mouse.get_pos()
+        clicked = pg.mouse.get_pressed()
+        if clicked:
+            for i in self.btnList:
+                print(mouse)
+                if i.image.get_width() + i.x > mouse[0] > i.x and i.image.get_height() + i.y > mouse[1] > i.y:
+                    print(self.btnList.index(i))
+
     def quit(self):
         self.save()
         pg.quit()
+
--- system.py
+++ system.py
@@ -3,6 +3,7 @@ import sys
 
 import pygame
 from npc import NPC
+from setting import Settings
 
 
 class Chapter:
@@ -34,13 +35,13 @@ class Chapter:
         self.clock.tick(150)
         for i in range(255):
             self.clock.tick(30)
-            self.screen.set_alpha(255-i)
+            self.screen.set_alpha(255 - i)
         self.background = pygame.image.load(self.backgrounds_path + ''.join(pos) + '.png')
-    
+
     def configure_npc(self, img, text):
         self.npcs.add(NPC(self.manager, self.screen, text, img))
 
-    def save(self, extra_data:dict):
+    def save(self, extra_data: dict = {}):
         data = {
             "pos": self.player,
             "npc": [[i.img, i.text] for i in self.npcs],
@@ -56,7 +57,15 @@ class Chapter:
             for i in data['npc']:
                 self.configure_npc(i[0], i[1])
             self.player.pos
-            
+
+    def draw_grid(self):
+        # 0부터 TILESIZE씩 건너뛰면서 WIDTH까지 라인을 그려준다
+        for x in range(0, self.screen.get_width(), 30):
+            # 첫번째 인자부터 game_world(게임 화면)에 (0,0,0,50)의 색으로 차례대로 라인을 그려준다
+            pygame.draw.line(self.screen, (0, 0, 0, 50), (x, 0), (x, self.screen.get_height()))
+        for y in range(0, self.screen.get_height(), 30):
+            pygame.draw.line(self.screen, (0, 0, 0, 50), (0, y), (self.screen.get_width(), y))
+
     def start(self):
         while True:
             self.clock.tick(60)
@@ -65,9 +74,13 @@ class Chapter:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.npcs.clear(self.screen, self.background)
+            self.npcs.update()
             self.npcs.draw(self.screen)
+            self.draw_grid()
             pygame.display.update()
 
     def stop(self):
         while True:
-            pass  # 게임 정지 및 게임 설정 띄우기
+            setting = Settings(self)
+            setting.open_setting()
+            # 게임 정지 및 게임 설정 띄우기
